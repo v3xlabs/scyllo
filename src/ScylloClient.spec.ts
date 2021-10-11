dfb318d0a7a75f4cc04ec7f7289b9978ccd53e77
Lucemans <luc@lucemans.nl>
2021-10-11 17:06:47 +0200

    v0.0.4

---
+++ src/ScylloClient.spec.ts
@@ -0,0 +1,33 @@
+import { ScylloClient } from ".";
+
+type User = {
+    username: string,
+    uid: number
+}
+
+type BlogPost = {
+    uid: number,
+    title: string,
+    description: string
+}
+
+(async () => {
+    const DB = new ScylloClient<{ 'users': User, 'blogposts': BlogPost }>({
+        client: {
+            contactPoints: [
+                'localhost:9042'
+            ],
+            'localDataCenter': 'datacenter1'
+        }
+    });
+
+    // await DB.useKeyspace('scyllo');
+
+    console.log('DONE');
+
+    // DB.client.execute('SELECT * FROM scyllo.users WHERE uid=?', [12345]);
+    // console.log(await DB.selectFrom('users', '*'));
+    // console.log(await DB.selectFrom('users', '*', {}));
+    // console.log(await DB.selectFrom('users', '*', { username: 'lucemans' }, 'ALLOW FILTERING'));
+
+})();
