62346f1948b627fc898f61f276b31c4803a61d8f
Lucemans <luc@lucemans.nl>
2021-10-11 18:37:16 +0200

    Improved testing

---
+++ __tests__/selectFrom.spec.ts
@@ -0,0 +1,40 @@
+import { ScylloClient } from "../lib";
+
+type User = {
+    username: string,
+    uid: number
+}
+
+let DB: ScylloClient<{ 'users': User }>;
+
+beforeAll(async () => {
+    DB = new ScylloClient({
+        client: {
+            contactPoints: [
+                'localhost:9042'
+            ],
+            'localDataCenter': 'datacenter1'
+        }
+    });
+    await DB.client.execute('SELECT * FROM system.local');
+});
+
+it('Can switch keyspace', async () => {
+    expect(DB.useKeyspace('scyllo'));
+});
+
+it('Can fetch every user from the database', async () => {
+    expect(await DB.selectFrom('users', '*')).toBeInstanceOf(Array);
+});
+
+it('Can fetch every user from the database with empty criteria object', async () => {
+    expect(await DB.selectFrom('users', '*', {})).toBeInstanceOf(Array);
+});
+
+it('Can request users using key', async () => {
+    console.log(await DB.selectFrom('users', '*', { uid: 1234567890 }, 'ALLOW FILTERING'));
+});
+
+it('Can request users using non-key and extra values', async () => {
+    console.log(await DB.selectFrom('users', '*', { username: 'lucemans' }, 'ALLOW FILTERING'));
+});
