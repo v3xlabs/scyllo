e9752fec12605684b872eaa9782a58c9e143b83a
Lucemans <luc@lucemans.nl>
2021-10-27 03:17:13 +0200

    Testing Upgrades

--- __tests__/createTableRaw.spec.ts
+++ __tests__/createTableRaw.spec.ts
@@ -1,9 +1,9 @@
 import { createTableRaw } from "../lib";
 
 it('Can create a basic table', async () => {
-    expect(createTableRaw('scyllo', 'atable', false, {a: {type: 'bigint'}}, 'a')).toEqual({"args": ["a", "bigint", "a"], "query": "CREATE TABLE scyllo.atable (? ?, PRIMARY KEY (?))"});
+    expect(createTableRaw<{'atable': {a:string}}, 'atable'>('scyllo', 'atable', false, {a: {type: 'bigint'}}, 'a')).toEqual({"args": ["a", "bigint", "a"], "query": "CREATE TABLE scyllo.atable (? ?, PRIMARY KEY (?))"});
 });
 
 it('Can create a basic table if not exists', async () => {
-    expect(createTableRaw('scyllo', 'atable', true, {a: {type: 'bigint'}}, 'a')).toEqual({"args": ["a", "bigint", "a"], "query": "CREATE TABLE IF NOT EXISTS scyllo.atable (? ?, PRIMARY KEY (?))"});
+    expect(createTableRaw<{'atable': {a:string}}, 'atable'>('scyllo', 'atable', true, {a: {type: 'bigint'}}, 'a')).toEqual({"args": ["a", "bigint", "a"], "query": "CREATE TABLE IF NOT EXISTS scyllo.atable (? ?, PRIMARY KEY (?))"});
 });
--- __tests__/insertIntoRaw.spec.ts
+++ __tests__/insertIntoRaw.spec.ts
@@ -1,5 +1,5 @@
 import { selectOneFromRaw } from "../lib";
 
 it('Can insert a user into the database', async () => {
-    expect(selectOneFromRaw('scyllo', 'users', '*', {uid: 1234567890, username: 'Jest'})).toEqual({query: 'SELECT * FROM scyllo.users WHERE uid=? AND username=? LIMIT 1', args: [1234567890, 'Jest']});
+    expect(selectOneFromRaw<{'users': {uid: number, username: string}}, 'users'>('scyllo', 'users', '*', {uid: 1234567890, username: 'Jest'})).toEqual({query: 'SELECT * FROM scyllo.users WHERE uid=? AND username=? LIMIT 1', args: [1234567890, 'Jest']});
 });
