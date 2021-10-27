80a01f1075de337e9d85f26f425c731ba8591d88
Lucemans <luc@lucemans.nl>
2021-10-27 02:24:35 +0200

    Introduce Query Execution Cleanup

--- src/ScylloClient.ts
+++ src/ScylloClient.ts
@@ -1,5 +1,5 @@
 import { Client, DseClientOptions, types } from 'cassandra-driver';
-import { createTableRaw, deleteFromRaw, insertIntoRaw } from './';
+import { createTableRaw, deleteFromRaw, insertIntoRaw, QueryBuild } from './';
 import { selectFromRaw, selectOneFromRaw, ValidDataType } from './QueryBuilder';
 
 export type DatabaseObject = {[key: string]: ValidDataType};
@@ -30,13 +30,20 @@ export class ScylloClient<TableMap extends Tables> {
         return await this.client.shutdown();
     }
 
-    async raw(cmd: string): Promise<types.ResultSet> {
-        return await this.client.execute(cmd);
+    async raw(query: string): Promise<types.ResultSet> {
+        if (this.debug)
+            console.log(`[Scyllo][Debug]\t${query}`);
+        return await this.client.execute(query);
     }
+
     async rawWithParams(query: string, args: any[]): Promise<types.ResultSet> {
         if (this.debug)
             console.log(`[Scyllo][Debug]\t${query}\n${args.reduce((a,b) => a + ' ' + b)}`);
         return await this.client.execute(query, args);
+    }
+    
+    async query(query: QueryBuild): Promise<types.ResultSet> {
+        return await this.rawWithParams(query.query, query.args);
     }
 
     async useKeyspace(keyspace: string) {
@@ -46,25 +53,25 @@ export class ScylloClient<TableMap extends Tables> {
 
     async selectFrom<F extends keyof TableMap>(table: F, select: '*' | (keyof TableMap[F])[], criteria?: { [key in keyof TableMap[F]]?: TableMap[F][key] | string }, extra?: string): Promise<TableMap[F][]> {
         const query = selectFromRaw<TableMap, F>(this.keyspace, table, select, criteria, extra);
-        const result = await this.rawWithParams(query.query, query.args);
+        const result = await this.query(query);
         return result.rows.map((row) => (Object.assign({}, ...row.keys().map(k => ({ [k]: row.get(k) }))))) as TableMap[F][];    
     }
 
     async selectOneFrom<F extends keyof TableMap>(table: F, select: '*' | (keyof TableMap[F])[], criteria?: { [key in keyof TableMap[F]]?: TableMap[F][key] | string }, extra?: string): Promise<TableMap[F]> {
         const query = selectOneFromRaw<TableMap, F>(this.keyspace, table, select, criteria, extra);
-        const result = await this.rawWithParams(query.query, query.args);
+        const result = await this.query(query);
         return result.rows.slice(0,1).map((row) => (Object.assign({}, ...row.keys().map(k => ({ [k]: row.get(k) })))))[0] as TableMap[F];    
     }
 
     async insertInto<F extends keyof TableMap>(table: F, obj: Partial<TableMap[F]>): Promise<types.ResultSet> {
         const query = insertIntoRaw<TableMap, F>(this.keyspace, table, obj);
-        const result = await this.rawWithParams(query.query, query.args);
+        const result = await this.query(query);
         return result;
     }
 
     async deleteFrom<F extends keyof TableMap>(table: F, fields: '*' | (keyof TableMap[F])[], criteria: { [key in keyof TableMap[F]]?: TableMap[F][key] | string }, extra?: string): Promise<types.ResultSet> {
         const query = deleteFromRaw<TableMap, F>(this.keyspace, table, fields, criteria, extra);
-        const result = await this.rawWithParams(query.query, query.args);
+        const result = await this.query(query);
         return result;
     }
 
@@ -78,7 +85,7 @@ export class ScylloClient<TableMap extends Tables> {
 
     async createTable<F extends keyof TableMap>(table: F, createIfNotExists: boolean, columns: {[key in keyof TableMap[F]]: {type: keyof typeof types.dataTypes}}, partition: [keyof TableMap[F], keyof TableMap[F]] | keyof TableMap[F], clustering?: (keyof TableMap[F])[]): Promise<types.ResultSet> {
         const query = createTableRaw(this.keyspace, table, createIfNotExists, columns, partition, clustering);
-        const result = await this.rawWithParams(query.query, query.args);
+        const result = await this.query(query);
         return result;
     }
 }
