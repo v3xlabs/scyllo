1431972f06cd775458993c527e0261eaa3716745
Luc <luc@lucemans.nl>
2021-12-27 13:31:34 +0100

    Improve generic naming (#13)

--- __tests__/createTableRaw.spec.ts
+++ __tests__/createTableRaw.spec.ts
@@ -1,9 +1,9 @@
 import { createTableRaw } from "../lib";
 
 it('Can create a basic table', async () => {
-    expect(createTableRaw<{'atable': {a:string}}, 'atable'>('scyllo', 'atable', false, {a: {type: 'bigint'}}, 'a')).toEqual({"args": ["a", "bigint", "a"], "query": "CREATE TABLE scyllo.atable (? ?, PRIMARY KEY (?))"});
+    expect(createTableRaw<{'atable': {a:string}}, 'atable'>('scyllo', 'atable', false, {a: {type: 'bigint'}}, 'a')).toEqual({"args": [], "query": "CREATE TABLE scyllo.atable (a bigint, PRIMARY KEY (a))"});
 });
 
 it('Can create a basic table if not exists', async () => {
-    expect(createTableRaw<{'atable': {a:string}}, 'atable'>('scyllo', 'atable', true, {a: {type: 'bigint'}}, 'a')).toEqual({"args": ["a", "bigint", "a"], "query": "CREATE TABLE IF NOT EXISTS scyllo.atable (? ?, PRIMARY KEY (?))"});
+    expect(createTableRaw<{'atable': {a:string}}, 'atable'>('scyllo', 'atable', true, {a: {type: 'bigint'}}, 'a')).toEqual({"args": [], "query": "CREATE TABLE IF NOT EXISTS scyllo.atable (a bigint, PRIMARY KEY (a))"});
 });
--- src/QueryBuilder.ts
+++ src/QueryBuilder.ts
@@ -1,6 +1,6 @@
 import { types } from 'cassandra-driver';
 
-import { Tables } from './ScylloClient';
+import { TableScheme } from './ScylloClient';
 import { toScyllo } from './ScylloTranslator';
 
 export type QueryBuild = {
@@ -8,34 +8,34 @@ export type QueryBuild = {
     args: any[]
 };
 
-export const selectFromRaw = <TableMap extends Tables, F extends keyof TableMap>(keyspace: string, table: F, select: '*' | (keyof TableMap[F])[], criteria?: { [key in keyof TableMap[F]]?: TableMap[F][key] | string }, extra?: string): QueryBuild => ({
+export const selectFromRaw = <TableMap extends TableScheme, F extends keyof TableMap>(keyspace: string, table: F, select: '*' | (keyof TableMap[F])[], criteria?: { [key in keyof TableMap[F]]?: TableMap[F][key] | string }, extra?: string): QueryBuild => ({
     query: `SELECT ${select == '*' ? select : select.join(',')} FROM ${keyspace}.${table} ${criteria && Object.keys(criteria).length > 0 ? ('WHERE ' + Object.keys(criteria).map(crit => crit + '=?').join(' AND ')) : ''} ${extra || ''}`.trim(),
     args: [...(criteria ? Object.values(criteria) : [])]
 });
 
-export const selectOneFromRaw = <TableMap extends Tables, F extends keyof TableMap>(keyspace: string, table: F, select: '*' | (keyof TableMap[F])[], criteria?: { [key in keyof TableMap[F]]?: TableMap[F][key] | string }, extra?: string): QueryBuild => ({
+export const selectOneFromRaw = <TableMap extends TableScheme, F extends keyof TableMap>(keyspace: string, table: F, select: '*' | (keyof TableMap[F])[], criteria?: { [key in keyof TableMap[F]]?: TableMap[F][key] | string }, extra?: string): QueryBuild => ({
     query: `SELECT ${select == '*' ? select : select.join(',')} FROM ${keyspace}.${table} ${criteria && Object.keys(criteria).length > 0 ? ('WHERE ' + Object.keys(criteria).map(crit => crit + '=?').join(' AND ')) : ''} LIMIT 1 ${extra || ''}`.trim(),
     args: [...(criteria ? Object.values(criteria) : [])]
 });
 
-export const insertIntoRaw = <TableMap extends Tables, F extends keyof TableMap>(keyspace: string, table: F, obj: Partial<TableMap[F]>): QueryBuild => ({
+export const insertIntoRaw = <TableMap extends TableScheme, F extends keyof TableMap>(keyspace: string, table: F, obj: Partial<TableMap[F]>): QueryBuild => ({
     query: `INSERT INTO ${keyspace}.${table} (${Object.keys(obj).join(', ')}) VALUES (${Object.keys(obj).map(() => '?').join(', ')})`,
     args: Object.values(obj).map(toScyllo)
 });
 
-export const updateRaw = <TableMap extends Tables, F extends keyof TableMap>(keyspace: string, table: F, obj: Partial<TableMap[F]>, criteria: { [key in keyof TableMap[F]]?: TableMap[F][key] | string }): QueryBuild => ({
+export const updateRaw = <TableMap extends TableScheme, TableName extends keyof TableMap, ColumnName extends keyof TableMap[TableName]>(keyspace: string, table: TableName, obj: Partial<TableMap[TableName]>, criteria: { [key in ColumnName]?: TableMap[TableName][key] | string }): QueryBuild => ({
     query: `UPDATE ${keyspace}.${table} SET ${Object.keys(obj).map(it => it + '=?').join(',')} ${criteria && Object.keys(criteria).length > 0 ? 'WHERE ' + Object.keys(criteria).map(crit => crit += '=?').join(' AND ') : ''}`.trim(),
     args: [...Object.values(obj), ...(criteria ? Object.values(criteria) : [])]
 });
 
-export const deleteFromRaw = <TableMap extends Tables, F extends keyof TableMap>(keyspace: string, table: F, fields: '*' | (keyof TableMap[F])[], criteria: { [key in keyof TableMap[F]]?: TableMap[F][key] | string }, extra?: string): QueryBuild => ({
+export const deleteFromRaw = <TableMap extends TableScheme, TableName extends keyof TableMap, ColumnName extends keyof TableMap[TableName]>(keyspace: string, table: TableName, fields: '*' | ColumnName[], criteria: { [key in ColumnName]?: TableMap[TableName][key] | string }, extra?: string): QueryBuild => ({
     query: `DELETE ${fields == '*' ? '' : fields.join(',')} ${keyspace}.${table} ${criteria && Object.keys(criteria).length > 0 ? ('WHERE ' + Object.keys(criteria).map(crit => crit + '=?').join(' AND ')) : ''} ${extra || ''}`.trim(),
     args: [...(criteria ? Object.values(criteria) : [])]
 });
 
 export type ColumnType = { type: keyof typeof types.dataTypes, test?: string };
 
-export const createTableRaw = <TableMap extends Tables, F extends keyof TableMap>(keyspace: string, table: F, createIfNotExists: boolean, columns: { [key in keyof TableMap[F]]: ColumnType }, partition: [keyof TableMap[F], keyof TableMap[F]] | keyof TableMap[F], clustering?: (keyof TableMap[F])[]): QueryBuild => ({
+export const createTableRaw = <TableMap extends TableScheme, F extends keyof TableMap>(keyspace: string, table: F, createIfNotExists: boolean, columns: { [key in keyof TableMap[F]]: ColumnType }, partition: [keyof TableMap[F], keyof TableMap[F]] | keyof TableMap[F], clustering?: (keyof TableMap[F])[]): QueryBuild => ({
     query: `CREATE TABLE${createIfNotExists ? ' IF NOT EXISTS' : ''} ${keyspace}.${table} (${(Object.keys(columns) as (keyof TableMap[F])[]).map((a) => a + ' ' + columns[a].type).join(',')}, PRIMARY KEY (${partition instanceof Array ? '(' + partition.join(',') + ')' : partition}${clustering ? `, ${clustering.join(',')}` : ''}))`,
     args: []
 });
