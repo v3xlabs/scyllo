fff74707dcd431f5ad74dd67815c726bfda693da
Jakob Helgesson <jakob.helgesson@gmail.com>
2022-01-01 14:28:21 +0100

    Remove forgotten example comment
    Co-authored-by: Luc <luc@lucemans.nl>

--- __tests__/createIndexRaw.spec.ts
+++ __tests__/createIndexRaw.spec.ts
@@ -27,11 +27,3 @@ it('Can create a basic table if not exists', async () => {
         query: 'CREATE INDEX IF NOT EXISTS buildings_index ON scyllo.buildings ((name), city)',
     });
 });
-
-// it('Can create a basic table', async () => {
-//     expect(createTableRaw<{'atable': {a:string}}, 'atable'>('scyllo', 'atable', false, {a: {type: 'bigint'}}, 'a')).toEqual({"args": [], "query": "CREATE TABLE scyllo.atable (a bigint, PRIMARY KEY (a))"});
-// });
-
-// it('Can create a basic table if not exists', async () => {
-//     expect(createTableRaw<{'atable': {a:string}}, 'atable'>('scyllo', 'atable', true, {a: {type: 'bigint'}}, 'a')).toEqual({"args": [], "query": "CREATE TABLE IF NOT EXISTS scyllo.atable (a bigint, PRIMARY KEY (a))"});
-// });
