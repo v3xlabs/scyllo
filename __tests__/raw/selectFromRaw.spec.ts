fde33e7573c31965034a04a2dfe7dac60e71f788
Elliot <me@robiot.dev>
2023-08-11 19:15:46 +0300

    Implement eqContains (#55)
    * Implement eqContains
    
    * Modify types for special expressions, correct types for eqContains and update tests
    
    ---------
    
    Co-authored-by: Antony1060 <antoniostignjedec@gmail.com>

--- __tests__/raw/selectFromRaw.spec.ts
+++ __tests__/raw/selectFromRaw.spec.ts
@@ -1,5 +1,4 @@
-import { selectFromRaw } from '../../lib';
-import { eqGreaterThanOrEqual, eqIn } from '../../lib/EqualityBuilder';
+import { selectFromRaw, eqGreaterThanOrEqual, eqIn, eqContains } from '../../lib';
 
 // fix
 it('Can insert a user into the database', async () => {
@@ -17,4 +16,8 @@ it('Can do number equality', async () => {
 
 it('Can do in equality', async () => {
     expect(selectFromRaw<{'users': {uid: number, username: string}}, 'users'>('scyllo', 'users', '*', {uid: 1, username: eqIn('antony', 'luc', 'jakob', 'elliot')}, 'ALLOW FILTERING')).toEqual({query: 'SELECT * FROM scyllo.users WHERE uid=? AND username in (?,?,?,?) ALLOW FILTERING', args: [1, 'antony', 'luc', 'jakob', 'elliot']});
+})
+
+it('Can do contains equality', async () => {
+    expect(selectFromRaw<{'users': {uid: number, roles: string[]}}, 'users'>('scyllo', 'users', '*', {uid: 1, roles: eqContains("admin")}, 'ALLOW FILTERING')).toEqual({query: 'SELECT * FROM scyllo.users WHERE uid=? AND roles contains ? ALLOW FILTERING', args: [1, 'admin']});
 })
